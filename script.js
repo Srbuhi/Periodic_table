74abd5045bcb1d8829dcca00d1949a9239a7b836
Srbuhi <srbuhi.sarg@gmail.com>
2024-08-13 23:05:24 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const highlightElements = document.querySelectorAll(".table-list-item");
+highlightElements[0].addEventListener("click", highlightBatteryElements);
+highlightElements[1].addEventListener("click", highlightBodyElements);
+highlightElements[2].addEventListener("click", highlightSeaElements);
+
+function highlightBatteryElements() {
+    const body = document.querySelector("body");
+    body.classList.remove("main", "human-body", "sea");
+    body.classList.add("batteries");
+}
+
+function highlightBodyElements() {
+    const body = document.querySelector("body");
+    body.classList.remove("main", "batteries", "sea");
+    body.classList.add("human-body");
+}
+
+function highlightSeaElements() {
+    const body = document.querySelector("body");
+    body.classList.remove("main", "batteries", "human-body");
+    body.classList.add("sea");
+}
+
+
+
