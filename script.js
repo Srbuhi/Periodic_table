01ba01ab5cefbb4c6987e3f1e34b84015199a50a
Srbuhi <srbuhi.sarg@gmail.com>
2024-08-15 01:35:51 +0400

    Highlight endangered elements

--- script.js
+++ script.js
@@ -3,23 +3,30 @@ const highlightElements = document.querySelectorAll(".tables-list-item");
 highlightElements[0].addEventListener("click", highlightBatteryElements);
 highlightElements[1].addEventListener("click", highlightBodyElements);
 highlightElements[2].addEventListener("click", highlightSeaElements);
+highlightElements[3].addEventListener("click", highlightEndangeredElements);
 
 function highlightBatteryElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "human-body", "sea");
+    body.classList.remove("main", "human-body", "sea", "endangered");
     body.classList.add("batteries");
 }
 
 function highlightBodyElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "batteries", "sea");
+    body.classList.remove("main", "batteries", "sea", "endangered");
     body.classList.add("human-body");
 }
 
 function highlightSeaElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "batteries", "human-body");
+    body.classList.remove("main", "batteries", "human-body", "endangered");
     body.classList.add("sea");
+}
+
+function highlightEndangeredElements() {
+    const body = document.querySelector("body");
+    body.classList.remove("main", "batteries", "human-body", "sea");
+    body.classList.add("endangered");
 }
 
 
