50e89efaf1bc609a8ca8f37c1825967ceff29c96
Srbuhi <srbuhi.sarg@gmail.com>
2024-08-16 01:59:40 +0400

    Highlight crust elements

--- script.js
+++ script.js
@@ -4,30 +4,36 @@ highlightElements[0].addEventListener("click", highlightBatteryElements);
 highlightElements[1].addEventListener("click", highlightBodyElements);
 highlightElements[2].addEventListener("click", highlightSeaElements);
 highlightElements[3].addEventListener("click", highlightEndangeredElements);
+highlightElements[4].addEventListener("click", highlightCrustElements);
 
 function highlightBatteryElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "human-body", "sea", "endangered");
+    body.classList.remove("main", "human-body", "sea", "endangered", "crust");
     body.classList.add("batteries");
 }
 
 function highlightBodyElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "batteries", "sea", "endangered");
+    body.classList.remove("main", "batteries", "sea", "endangered", "crust");
     body.classList.add("human-body");
 }
 
 function highlightSeaElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "batteries", "human-body", "endangered");
+    body.classList.remove("main", "batteries", "human-body", "endangered", "crust");
     body.classList.add("sea");
 }
 
 function highlightEndangeredElements() {
     const body = document.querySelector("body");
-    body.classList.remove("main", "batteries", "human-body", "sea");
+    body.classList.remove("main", "batteries", "human-body", "sea", "crust");
     body.classList.add("endangered");
 }
 
+function highlightCrustElements() {
+    const body = document.querySelector("body");
+    body.classList.remove("main", "batteries", "human-body", "sea", "endangered");
+    body.classList.add("crust");
+}
 
 
