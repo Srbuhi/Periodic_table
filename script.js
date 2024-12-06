18308cdf84c809db5e533c638aa1fa77049cfbf8
Srbuhi <srbuhi.sarg@gmail.com>
2024-12-07 02:19:56 +0400

    Improve hovering

--- script.js
+++ script.js
@@ -7,17 +7,37 @@ highlightElements[3].addEventListener("click", highlightEndangeredElements);
 highlightElements[4].addEventListener("click", highlightCrustElements);
 highlightElements[5].addEventListener("click", highlightPriceOfElements);
 
-const iconMeaning = document.querySelectorAll(".icon-meaning");
-highlightElements.forEach((icon, index) => icon.addEventListener("mouseenter", showHint));
-highlightElements.forEach((icon, index) => icon.addEventListener("mouseleave", hideHint));
+// const iconMeaning = document.querySelectorAll(".icon-meaning");
+// highlightElements.forEach((icon, index) => icon.addEventListener("mouseenter", showHint));
+// highlightElements.forEach((icon, index) => icon.addEventListener("mouseleave", hideHint));
 
-function showHint() {
-    iconMeaning[1].style.display = "block";
+// function showHint() {
+//     iconMeaning[1].style.display = "block";
+// }
+
+// function hideHint() {
+//     iconMeaning[1].style.display = "none";
+// }
+const hints = {
+    "battery"    : "Elements in batteries",
+    "human-body" : "Elements in human body",
+    "sea"        : "Elements in sea",
+    "warning"    : "Endangered elements",
+    "earth-crust": "Elements is Earth's crust",
+    "price"      : "Elements prices"
 }
 
-function hideHint() {
-    iconMeaning[1].style.display = "none";
-}
+const iconMeaning = document.querySelector(".icon-meaning");
+highlightElements.forEach(icon => {
+    icon.addEventListener("mouseenter", () => {
+      const hintKey = [...icon.classList].find(className => hints[className]); //find method returns the first element satisfying provided function
+      if(hintKey) {
+        iconMeaning.textContent = hints[hintKey];
+        iconMeaning.style.display = "block";
+      }
+    })
+    icon.addEventListener("mouseleave", () => iconMeaning.style.display = "none")
+})
 
 function highlightBatteryElements() {
     const body = document.querySelector("body");
